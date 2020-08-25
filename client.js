6a714d0ee8b14ad76608f97ebe43b1aee00ed178
Ramya-Geethika <geethikakramya@gmail.com>
2020-08-25 21:02:04 +0000

    Playing with snake

---
+++ client.js
@@ -0,0 +1,30 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '10.0.2.15',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write("Name: KRG");
+    
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  
+  return conn;
+}
+console.log('Connecting ...');
+connect();
+
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-
 const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', (message) => {console.log('server send : ${message}')} )
 
-  return conn;
-};
-
-connect();
+const { connect } = require('./client');
